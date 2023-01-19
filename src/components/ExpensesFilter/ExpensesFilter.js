c9f8dbe2d1b2788cbdf6614e22e613ab950a4ade
riahamhari <109351021+riahamhari@users.noreply.github.com>
2023-01-19 15:55:29 +0000

    updated date options

--- src/components/ExpensesFilter/ExpensesFilter.js
+++ src/components/ExpensesFilter/ExpensesFilter.js
@@ -19,10 +19,10 @@ const ExpensesFilter = (props) => {
                     value={props.selected}
                     onChange={selectChangeHandler}
                 >
+                    <option value='2023'>2023</option>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
-                    <option value='2019'>2019</option>
                 </select>
             </div>
         </div>
